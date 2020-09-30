961c27fa8eca2e931ce69355a5044fb6bd361ffb
澄清YiFang <lovestory52012@gmail.com>
2020-09-30 14:05:07 +0800

    Create 0930.cpp

---
+++ role/0930.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+struct Role{
+char *name;
+int level,hp,ap,dp,sp;
+
+};
+
+void test_role(){
+	char name[]="路人甲";
+	struct Role role;
+	role.name=name;
+	role.level=1;
+	role.hp=10;
+	role.ap=1;
+	role.dp=1;
+	role.sp=5;
+	void dump_role(Role&role); 
+}
+
+void dump_role(Role&role){
+	cout<<"名稱："<<role.name<<"\n";
+	cout<<"等級："<<role.level<<"\n";
+	cout<<"血量："<<role.hp<<"\n";
+	cout<<"攻擊："<<role.ap<<"\n";	
+	cout<<"防禦："<<role.dp<<"\n";
+	cout<<"精神："<<role.sp<<"\n"; 
+	 
+}
+
+
+
+
+

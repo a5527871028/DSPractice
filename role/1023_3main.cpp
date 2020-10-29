5118ec0ce5a5d58b324743a2dfde15fd01305bf8
澄清YiFang <lovestory52012@gmail.com>
2020-10-30 01:04:59 +0800

    Update 1023_3main.cpp

--- role/1023_3main.cpp
+++ role/1023_3main.cpp
@@ -7,28 +7,20 @@ get(i, j): 傳回二維陣列第 i 列，第 j 行的元素值。
 set(i, j, v): 設定二維陣列第 i 列，第 j 行的元素值為 v。 
 column行，Row列。 
 */
+    int i,j,v=3;
     
 
-
 void dump(){
-	int iArray[12] = {1,2,3,4,5,6,7,8,9,10,11};
-	int i,j,v;
-	iArray[i,j]=iArray[1];
-	cout<<"題目1:[0,1]="<<iArray[i,j]<<"\n以下是所有排列\n\n";
+	int iArray[12] = {1,2,3,4,
+	                  5,6,7,8,
+					  9,10,11,12};	
+	iArray[i,j]=iArray[0];
+	cout<<"題目1:[0,0]="<<iArray[i,j]<<"\n\n";
+	
+	iArray[i,j]=v;
+	cout<<"題目2:[0,0]="<<iArray[i,j]<<"\n";
 	
 	
-	for(i=0;i<5;i++){
-		for(j=0;j<4;j++){
-		cout<<iArray[i]<<"\t";
-		iArray[i]=iArray[i]+5;
-		
-		}
-		cout<<endl;	
-	}
-	cout<<endl<<endl;
-	
-	iArray[i,j]=iArray[2,3]=7;
-	cout<<"題目2更換後[2,3]="<<iArray[i,j]<<"\n\n"; 
 	
 	
 }

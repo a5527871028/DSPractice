16bd1eb7b085f5fc136b80d569e67874fd48ec4d
澄清YiFang <lovestory52012@gmail.com>
2020-11-29 11:27:58 +0800

    Update stack_1113.cpp

--- stack_1127/stack_1113.cpp
+++ stack_1127/stack_1113.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Stack{
 	private:
 		int *data;
-		int top_; //堆疊索引 ，第一項作業：int main時要先印出來頂端元素 
+		int top_; //堆疊索引 ，第一項作業：int main時要印出來頂端元素 
 	public:
 		Stack(int size); //Constructor 
 		~Stack();   //Destructor，作業第三項：清空堆疊內容
@@ -28,7 +28,7 @@ Stack::Stack(int size){
 }
 
 Stack::~Stack(){
-	cout<<"堆疊銷毀"<<"\n"; 
+	cout<<"\n"<<"堆疊銷毀。"<<"\n"; 
 }
 
 bool Stack::isEmpty(void){
@@ -60,7 +60,7 @@ Stack::pop(){
 
 int main(int argc, char** argv){
 	int i=0,S=8;
-	int a[10]={0,1,2,3,4,5,6,7,8,9};
+	int a[10]={0,1,2,3,4,5,6,7,8,9};//data
 	
 	Stack stack(S);
 	
@@ -70,16 +70,16 @@ int main(int argc, char** argv){
 		stack.push(a[i]);
 		i++; 
 	}
-
-	cout<<"\t"<<"印出第五個值"<<stack.peek(a[5]) ;
+	
+	cout<<"\n"<<endl;
+    cout<<"\t"<<"印出第五個值"<<stack.peek(a[5]) ;
 	cout<<"\t"<<"最頂端的值為"<<stack.top();
 	
 	i=0;
-	cout<<"\n\n"<<"取出資料"<<"\n";
-	if(stack.isEmpty())
-		cout<<i++<<"取出"<<stack.pop(); 
-	else
-	    cout<<"空的陣列";
+	cout<<"\n\n"<<"取出資料:"<<"\t";
+	if(!stack.isEmpty())
+		cout<<i++<<"到"<<stack.pop()<<"取出"; 
+	
 	
 	
 }

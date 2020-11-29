3d5d8ba2b694729219ae36c7fb5388e32dcc0c59
澄清YiFang <lovestory52012@gmail.com>
2020-11-29 11:09:26 +0800

    Update stack_1113.cpp

--- stack_1127/stack_1113.cpp
+++ stack_1127/stack_1113.cpp
@@ -7,15 +7,17 @@ class Stack{
 		int top_; //堆疊索引 ，第一項作業：int main時要先印出來頂端元素 
 	public:
 		Stack(int size); //Constructor 
-		~Stack();   //Destructor，作業第三項：清空堆疊內容,在解構前main中使用Stack.clear(); 
+		~Stack();   //Destructor，作業第三項：清空堆疊內容
 		
-		int peep(int i);//類似top，作業第二項：偷看堆疊第某個元素 
+		
 
 		bool isEmpty(); //空串列，作業第四項：判斷是否為空 
 		bool full(int size); //滿溢 
 		void push(int key); //DATA放入堆疊 
+		int peek(int i);//類似top，作業第二項：偷看堆疊第某個元素 
+		int top(); //查看頂端資料
 		int pop(); //將頂端資料取出	
-		int top();	
+			
 		
 }; 
 
@@ -41,6 +43,10 @@ void Stack::push(int key){
 	data[++top_] = key;
 }
 
+Stack::peek(int i){
+	return i;
+} 
+
 Stack::top(){
 	return data[top_];
 }
@@ -48,7 +54,7 @@ Stack::top(){
 Stack::pop(){
 	int temp;
 	temp=data[top_];
-	top--;
+	top_--;
 	return temp;
 }
 
@@ -60,16 +66,17 @@ int main(int argc, char** argv){
 	
 	cout<<"\n"<<"資料放入"<<"\n";
 	while(i<10&&!stack.full(S)) {
-		cout<<i<<"放"<<i<<"\t";
+		cout<<i<<"放"<<a[i]<<"\t";
 		stack.push(a[i]);
 		i++; 
 	}
-	
+
+	cout<<"\t"<<"印出第五個值"<<stack.peek(a[5]) ;
 	cout<<"\t"<<"最頂端的值為"<<stack.top();
 	
 	i=0;
 	cout<<"\n\n"<<"取出資料"<<"\n";
-	if(!stack.isEmpty())
+	if(stack.isEmpty())
 		cout<<i++<<"取出"<<stack.pop(); 
 	else
 	    cout<<"空的陣列";

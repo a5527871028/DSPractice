8c68d30ba95b735f5ee6499fd69e763bc2666149
澄清YiFang <lovestory52012@gmail.com>
2020-12-04 01:51:03 +0800

    Create 1023_3main.cpp

---
+++ array_1023/1023_3main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+題目：
+get(i, j): 傳回二維陣列第 i 列，第 j 行的元素值。
+set(i, j, v): 設定二維陣列第 i 列，第 j 行的元素值為 v。 
+column行，Row列。 
+*/
+    int i,j,v=3;
+    
+
+void dump(){
+	int iArray[12] = {1,2,3,4,
+	                  5,6,7,8,
+					  9,10,11,12};	
+	iArray[i,j]=iArray[0];
+	cout<<"題目1:[0,0]="<<iArray[i,j]<<"\n\n";
+	cout<<"v="<<v<<endl<<"將[0,0]值設為v值\n";
+	iArray[i,j]=v;
+	cout<<"\n題目2:[0,0]="<<iArray[i,j]<<"\n";
+	
+	
+	
+	
+}
+
+int main(int argc, char** argv) {
+	dump();
+	return 0;
+}
